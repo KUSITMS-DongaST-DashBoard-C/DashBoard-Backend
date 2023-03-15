d178a06edbbd3ffc29580d640707381188418ca4
Jinwoo Chang <jenny010925@naver.com>
2023-03-15 19:50:21 +0900

    test

--- src/main/java/com/example/dashboardback/DashboardbackApplication.java
+++ src/main/java/com/example/dashboardback/DashboardbackApplication.java
@@ -8,6 +8,10 @@ public class DashboardbackApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DashboardbackApplication.class, args);
+
+		// 메모리 사용량 출력
+		long heapSize = Runtime.getRuntime().totalMemory();
+		System.out.println("HEAP Size(M) : "+ heapSize / (1024*1024) + " MB");
 	}
 
 }
