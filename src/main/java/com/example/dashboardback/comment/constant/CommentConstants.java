63fe8c88d64b0802254431320a5232f13bc5d3fb
Minsu <syt07100@gmail.com>
2023-03-17 12:26:33 +0900

    [Feat] 메모의 댓글 생성, 삭제, 조회

---
+++ src/main/java/com/example/dashboardback/comment/constant/CommentConstants.java
@@ -0,0 +1,27 @@
+package com.example.dashboardback.comment.constant;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+public class CommentConstants {
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum ECommentResponseMessage{
+        CREATE_COMMENT_SUCCESS("댓글을 작성했습니다."),
+        DELETE_COMMENT_SUCCESS("댓글을 삭제했습니다."),
+        GET_ALL_DETAIL_COMMENTS_SUCCESS("메모의 모든 댓글을 조회했습니다.");
+        private final String message;
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum CommentExceptionList {
+        NOT_FOUND_COMMENT("C0001", HttpStatus.NOT_FOUND, "해당 commentId로 Comment를 찾을 수 없습니다.");
+
+        private final String errorCode;
+        private final HttpStatus httpStatus;
+        private final String message;
+    }
+}
--- src/main/java/com/example/dashboardback/user/dto/UserDto.java
+++ src/main/java/com/example/dashboardback/user/dto/UserDto.java
@@ -9,8 +9,6 @@ import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotBlank;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public abstract class UserDto {
