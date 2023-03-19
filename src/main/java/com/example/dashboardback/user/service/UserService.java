1142c036c7471aa9fb48fcf3d057e9be6f6e4490
Minsu <syt07100@gmail.com>
2023-03-19 19:07:40 +0900

    [FEAT] 유저 더미데이터 수정

--- src/main/java/com/example/dashboardback/user/service/UserService.java
+++ src/main/java/com/example/dashboardback/user/service/UserService.java
@@ -1,6 +1,9 @@
 package com.example.dashboardback.user.service;
 
+import com.example.dashboardback.user.entity.User;
+
 public interface UserService {
 
     void makeUserData();
+    void updateCreatedAtWithRandomDate(int daysAgo,int loginDaysAgo);
 }
